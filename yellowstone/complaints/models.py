bdf7da35a3ed48930ed850deb492e9d71cc6c215
Parth Verma <v.parth98@gmail.com>
2017-02-01 20:35:01 +0530

    Added model for complaint types

--- yellowstone/complaints/models.py
+++ yellowstone/complaints/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from autoslug import AutoSlugField
 # Create your models here.
+
+class Complaint_Types(models.Model):
+    name = models.CharField(max_length=50)
+    slug = AutoSlugField(max_length=50,populate_from=name, unique=True)
+
+    class Meta:
+        default_permissions = ('add', 'change', 'delete', 'view')
+        ordering = ('slug',)
+        db_table = 'complaint_types'
