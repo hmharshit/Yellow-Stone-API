4c6ec111d81eb1e71c4cf1e775e6b6e8c7c18a6a
Parth Verma <v.parth98@gmail.com>
2017-02-02 15:16:04 +0530

    Updated user model

--- yellowstone/user/models.py
+++ yellowstone/user/models.py
@@ -3,12 +3,10 @@ from django.core.validators import RegexValidator
 # Create your models here.
 
 class Passenger(models.Model):
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-    email = models.EmailField(max_length=50)
+    name = models.CharField(max_length = 100)
+    email = models.EmailField(max_length=50,unique=True)
     phone_regex = RegexValidator(regex=r'^\+?9?1?([7-9]|1)?\d{9}$', message="Invalid phone number")
     phone_number = models.CharField(validators=[phone_regex], blank=True, max_length=15)
-    gender = models.CharField(max_length=6)
 
     def __str__(self):
         return self.first_name + self.last_name
