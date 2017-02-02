b2cf48e99a39f38d2f1bb66e116d274eb985bc94
root <root@localhost.localdomain>
2017-02-02 11:52:39 +0530

    Fixed stations model

--- yellowstone/stations/models.py
+++ yellowstone/stations/models.py
@@ -3,9 +3,9 @@ from autoslug import AutoSlugField
 # Create your models here.
 
 
-class Station(models.Models):
-    station_id = models.IntField(required=True, unique=True)
-    name = models.CharField(max_length=100, required =True)
-    slug = AutoSlugField(max_length=100, populate_from=name,unique=True)
-    state = models.CharField(max_length=30, required=True)
-    city = models.CharField(max_length=30, required=True)
+class Station(models.Model):
+    station_id = models.IntegerField(null=False, unique=True)
+    name = models.CharField(max_length=100, null=True)
+    slug = AutoSlugField(max_length=100, populate_from=name, unique=True)
+    state = models.CharField(max_length=30, null=True)
+    city = models.CharField(max_length=30, null=True)
