5ceeb45f399715750da481bb7cf85b3769cf90f8
Parth Verma <v.parth98@gmail.com>
2017-02-06 09:31:07 +0530

    Added all models to admin site

--- yellowstone/complaints/admin.py
+++ yellowstone/complaints/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from complaints.models import ComplaintCategory,ComplaintSubCategory
 
 # Register your models here.
+
+admin.site.register(ComplaintCategory)
+admin.site.register(ComplaintSubCategory)
--- yellowstone/suggestion/admin.py
+++ yellowstone/suggestion/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from suggestion.models import SuggestionCategory,SuggestionSubCategory
 # Register your models here.
+
+admin.site.register(SuggestionCategory)
+admin.site.register(SuggestionSubCategory)
