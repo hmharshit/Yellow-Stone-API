970ab3260276655fec22bc12cbb62a39694683d8
Parth Verma <v.parth98@gmail.com>
2017-02-08 09:45:02 +0530

    WIP: Suggestions sub category api

--- yellowstone/suggestion/models.py
+++ yellowstone/suggestion/models.py
@@ -3,27 +3,29 @@ from autoslug import AutoSlugField
 # Create your models here.
 
 class SuggestionCategory(models.Model):
-    category_id = models.IntegerField(unique = True, primary_key = True)
     name = models.CharField(max_length=50)
     slug = AutoSlugField(max_length=50,populate_from='name', unique=True)
 
     class Meta:
         default_permissions = ('add', 'change', 'delete', 'view')
         ordering = ('slug',)
-        db_table = 'suggestion_types'
+        db_table = 'suggestiontypes'
 
     def __str__(self):
         return self.name
 
 class SuggestionSubCategory(models.Model):
-    sub_category_id = models.IntegerField()
-    category_id = models.IntegerField()
+
+    suggestion_category = models.ForeignKey(
+        SuggestionCategory, null=False, blank=False, on_delete=models.CASCADE, related_name='xyz_comp')
     name = models.CharField(max_length=60)
     slug = AutoSlugField(max_length=60, populate_from='name', unique=True)
 
     class Meta:
-        unique_together = ('category_id', 'sub_category_id')
-        db_table = 'suggestion_sub_types'
+        unique_together = ('suggestion_category', 'id')
+        db_table = 'suggestion_subtypes'
+        default_permissions = ('add', 'change', 'delete', 'view')
+        ordering = ('slug',)
 
     def __str__(self):
         return self.name
