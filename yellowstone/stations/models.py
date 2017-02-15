eddc0752c20aea8e3461979567acd20662372ac8
Parth Verma <v.parth98@gmail.com>
2017-02-15 20:21:07 +0530

    Fixed Stations model issue

--- yellowstone/stations/models.py
+++ yellowstone/stations/models.py
@@ -8,7 +8,7 @@ class Station(models.Model):
     latitude = models.DecimalField(max_digits=11, decimal_places=7)
     longitude = models.DecimalField(max_digits=11, decimal_places=7)
     name = models.CharField(max_length=100, null=False)
-    slug = AutoSlugField(max_length=100, populate_from=name, unique=True)
+    slug = AutoSlugField(max_length=100, populate_from='name', unique=True)
     state = models.CharField(max_length=30, null=False)
     city = models.CharField(max_length=30, null=False)
     area = models.CharField(max_length=20)
